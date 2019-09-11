86d64daacde366f325eab3af2d7eef0fb37103e8
Zihan Chen <vczh@163.com>
2019-09-10 22:11:30 -0700

    ...

--- starter-packages/starter-lit-html/src/indexView.ts
+++ starter-packages/starter-lit-html/src/indexView.ts
@@ -1,3 +1,15 @@
-export function something(): void {
-    // nothing
+import { html, render, TemplateResult } from 'lit-html';
+
+class IndexView {
+    constructor(public title: string) {
+    }
+
+    public getTemplate(): TemplateResult {
+        return html`Hello, <strong>${this.title}</strong>`;
+    }
+}
+
+export function renderTemplate(title: string, target: Element): void {
+    const view = new IndexView(title);
+    render(view.getTemplate(), target);
 }
