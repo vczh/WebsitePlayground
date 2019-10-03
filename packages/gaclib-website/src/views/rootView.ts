c24452647a0a432665742915e15c41fa23125660
Zihan Chen <vczh@163.com>
2019-10-03 01:38:29 -0700

    Update rootView.ts

--- packages/gaclib-website/src/views/rootView.ts
+++ packages/gaclib-website/src/views/rootView.ts
@@ -1,5 +1,14 @@
 import { html, render } from 'lit-html';
 
+const buttons: [string, string, string][] = [
+    ['Home', 'index', 'HOME'],
+    ['Tutorial', 'tutorial', 'TUTORIAL'],
+    ['Demo', 'demo', 'DEMOES'],
+    ['Download', 'download', 'DOWNLOAD'],
+    ['Document', 'document', 'DOCUMENT'],
+    ['Contact', 'contact', 'CONTACT']
+];
+
 export const viewExport = {
     renderView(model: {}, target: Element): void {
         const htmlTemplate = html`
@@ -12,37 +21,17 @@ export const viewExport = {
                         <img src="/logo.gif" />
                     </td>
                 </tr>
-                <tr>
-                    <td align="center" valign="middle">
-                        <a id="navHome" class="MenuButton HomeButton" href="/index.html">
-                            HOME
-                        </a>
-                    </td>
-                    <td align="center" valign="middle">
-                        <a id="navTutorial" class="MenuButton TutorialButton" href="/tutorial.html">
-                            TUTORIAL
-                        </a>
-                    </td>
-                    <td align="center" valign="middle">
-                        <a id="navDemo" class="MenuButton DemoButton" href="/demo.html">
-                            DEMOES
-                        </a>
-                    </td>
-                    <td align="center" valign="middle">
-                        <a id="navDownload" class="MenuButton DownloadButton" href="/download.html">
-                            DOWNLOAD
-                        </a>
-                    </td>
-                    <td align="center" valign="middle">
-                        <a id="navDocument" class="MenuButton DocumentButton" href="./document.html">
-                            DOCUMENT
-                        </a>
-                    </td>
-                    <td align="center" valign="middle">
-                        <a id="navContact" class="MenuButton ContactButton" href="/contact.html">
-                            CONTACT
-                        </a>
-                    </td>
+                <tr>${
+            buttons.map((button: [string, string, string]) => {
+                const [htmlClass, htmlLink, htmlText] = button;
+                return html`
+                <td align="center" valign="middle">
+                    <a id="nav${htmlClass}" class="MenuButton ${htmlClass}Button" href="/${htmlLink}.html">
+                        ${htmlText}
+                    </a>
+                </td>
+                `;
+            })}
                 </tr>
                 <tr>
                     <td align="left" valign="top" colspan="6">
