700ec356055a36f767bf31739858d33731616ad2
Zihan Chen <vczh@163.com>
2019-10-03 21:31:17 -0700

    Update TestDeserialization.ts

--- packages/gaclib-article/test/TestDeserialization.ts
+++ packages/gaclib-article/test/TestDeserialization.ts
@@ -40,3 +40,51 @@ test(`Article with attributes`, () => {
     };
     assert.deepStrictEqual(parseArticle(input), output);
 });
+
+test(`Nested topics`, () => {
+    const input = `
+<article>
+    <topic>
+        <title>Article</title>
+        <topic><title>1.h2</title></topic>
+        <topic>
+            <topic><title>2.1.h3</title></topic>
+            <topic><title>2.2.h3</title></topic>
+            <title>2.h2</title>
+        </topic>
+    </topic>
+</article>
+`;
+    const output: Article = {
+        index: false,
+        numberBeforeTitle: false,
+        topic: {
+            kind: 'Topic',
+            title: 'Article',
+            content: [
+                {
+                    kind: 'Topic',
+                    title: '1.h2',
+                    content: []
+                },
+                {
+                    kind: 'Topic',
+                    title: '2.h2',
+                    content: [
+                        {
+                            kind: 'Topic',
+                            title: '2.1.h3',
+                            content: []
+                        },
+                        {
+                            kind: 'Topic',
+                            title: '2.2.h3',
+                            content: []
+                        }
+                    ]
+                }
+            ]
+        }
+    };
+    assert.deepStrictEqual(parseArticle(input), output);
+});
